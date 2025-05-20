42a0df9d18a2e8c503c37b5dd4a99d3c7eb7dd6e
amaral1703 <gamaral987654321@gmail.com>
2025-05-19 22:10:32 -0300

    Add migration script to create Products table

---
+++ migrations/20250514230418-create-product.js
@@ -0,0 +1,47 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: true
+      },
+      sku: {
+        type: Sequelize.TEXT,
+        unique: true,
+        allowNull: false
+      },
+      price: {
+        type: Sequelize.REAL,
+        allowNull: false,
+        defaultValue: 0.0
+      },
+      qr_text: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Produtos');
+  }
+};
